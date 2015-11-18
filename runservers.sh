4103e13fc4d82081aae219ebb1ed076df3bbc30a
Hosam Shahin <hosamlshahin@gmail.com>
2015-11-17 20:03:53 -0500

    update runservers.sh

--- runservers.sh
+++ runservers.sh
@@ -1,20 +1,17 @@
-# Function to issue sudo command with password
-function sudo-pw {
-    echo "vagrant" | sudo -S $@
-}
-
-# OpenDSA-server
-cd /vagrant/OpenDSA-server/ODSA-django
-sudo-pw kill `sudo lsof -t -i:8443`
-sudo-pw kill `sudo lsof -t -i:8001`
-setsid sudo-pw ./runserver
-
 # OpenDSA content server
 cd /vagrant/OpenDSA
-sudo-pw kill `sudo lsof -t -i:8080`
-setsid ./WebServer
+sudo kill `sudo lsof -t -i:8080`
+(setsid ./WebServer &)
 
+sleep 5
 # OpenDSA-LTI server
 cd /vagrant/OpenDSA-LTI
-sudo-pw kill `sudo lsof -t -i:9292`
-setsid sudo-pw rackup config.ru
+sudo kill `sudo lsof -t -i:9292`
+(setsid sudo rackup config.ru &)
+
+sleep 5
+# OpenDSA-server
+cd /vagrant/OpenDSA-server/ODSA-django
+sudo kill `sudo lsof -t -i:8443`
+sudo kill `sudo lsof -t -i:8001`
+(setsid sudo ./runserver &)
