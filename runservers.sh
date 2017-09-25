88fea8c608aa527e3abb0567db9da4e92d233ecc
Hossameldin Shahin <hshahin@vt.edu>
2017-09-25 15:42:16 -0400

    Add bundle exec when running thin

--- runservers.sh
+++ runservers.sh
@@ -5,7 +5,7 @@ cd /vagrant/OpenDSA
 sleep 2
 # OpenDSA-LTI server
 cd /vagrant/OpenDSA-LTI
-(setsid thin start --ssl --ssl-key-file server.key --ssl-cert-file server.crt -p 9292 &)
+(setsid bundle exec thin start --ssl --ssl-key-file server.key --ssl-cert-file server.crt -p 9292 &)
 
 sleep 2
 # OpenDSA-LTI server
